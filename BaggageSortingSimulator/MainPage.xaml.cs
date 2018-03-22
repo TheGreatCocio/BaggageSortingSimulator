a3e061f8cc326b99acf04444ac4c1586d6272b6e
David Jessen <jesse@DESKTOP-TROP5QP>
2018-03-22 12:39:10 +0100

    Added a send to terminal

--- BaggageSortingSimulator/MainPage.xaml.cs
+++ BaggageSortingSimulator/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -26,7 +27,9 @@ namespace BaggageSortingSimulator
         {
             this.InitializeComponent();
             
+
         }
+        
         
     }
 }
--- BaggageSortingSimulator/SortingMachine.cs
+++ BaggageSortingSimulator/SortingMachine.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BaggageSortingSimulator.System;
 
 namespace BaggageSortingSimulator
 {
     public class SortingMachine
     {
         private static SortingMachine instance;
-
+        private DAL dal = new DAL();
+        List<string> Terminals = new List<string>();
         public static SortingMachine Instance
         {
             get
@@ -24,6 +26,53 @@ namespace BaggageSortingSimulator
         private SortingMachine()
         {
 
+        }
+        public void SendToTerminal(int terminalNo)
+        {
+            
+            
+            
+            switch (terminalNo)
+            {
+                case 0:
+                    Terminals.Add("Not Active");
+                    break;
+                case 1:
+                    Terminals.Add(dal.GetDestination(0));
+                    break;
+                case 2:
+                    Terminals.Add(dal.GetDestination(1));
+                    break;
+                case 3:
+                    Terminals.Add(dal.GetDestination(2));
+                    break;
+                case 4:
+                    Terminals.Add(dal.GetDestination(3));
+                    break;
+                case 5:
+                    Terminals.Add(dal.GetDestination(4));
+                    break;
+                case 6:
+                    Terminals.Add(dal.GetDestination(5));
+                    break;
+                case 7:
+                    Terminals.Add(dal.GetDestination(6));
+                    break;
+                case 8:
+                    Terminals.Add(dal.GetDestination(7));
+                    break;
+                    
+            }
+
+        }
+        public string Test()
+        {
+            string test = "";
+            foreach (string t in Terminals)
+            {
+                test=t;
+            }
+            return test;
         }
     }
     
