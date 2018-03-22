dfcf16c434fe6a2f7f0af128937ec328543ffbd3
Mark Fog <TheBigCocio@outlook.dk>
2018-03-22 09:23:25 +0100

    Terminal&Counter

--- BaggageSortingSimulator/Model/Counter.cs
+++ BaggageSortingSimulator/Model/Counter.cs
@@ -8,5 +8,14 @@ namespace BaggageSortingSimulator.Model
 {
     class Counter
     {
+        private static int counterIncrementer = 1;
+        private int counterId;
+
+        public int CounterId { get => counterId; set => counterId = value; }
+
+        public Counter()
+        {
+            counterId = counterIncrementer++;
+        }
     }
 }
--- BaggageSortingSimulator/Model/Terminal.cs
+++ BaggageSortingSimulator/Model/Terminal.cs
@@ -8,5 +8,6 @@ namespace BaggageSortingSimulator.Model
 {
     class Terminal
     {
+
     }
 }
